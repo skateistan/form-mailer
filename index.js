66c868b5996da15d25a01613c454ad7c772d4825
manue1 <manuel.bergler@gmail.com>
2017-05-01 14:22:52 -0500

    Add Sendgrid substitions. SendMail works.

--- index.js
+++ index.js
@@ -18,12 +18,10 @@ server.route({
   method: 'POST',
   path:'/sendmail',
   handler: function (request, reply) {
-    console.log('body', request.payload);
-    sendMail(request.payload)
+    console.log('Request payload: ', request.payload);
+    return sendMail(request.payload)
       .then(api_res => reply('').code(api_res.status))
       .catch(api_err => reply('').code(api_err.status))
-
-    return reply('sent');
   }
 }
 );
@@ -46,7 +44,7 @@ function verifyCaptcha(req){
       .set('Content-Type', 'application/json')
       .send(params)
       .end((err, res)=>{
-        console.log('res', res);
+        console.log('Recaptcha - res statusCode', res.statusCode);
         if (err) fnReject(err);
         else res.statusCode === 200 ? fnResolve(res.statusCode) : fnReject(res.statusCode);
       });
@@ -54,23 +52,37 @@ function verifyCaptcha(req){
 }
 
 function sendMail(params) {
-  var compose = {
+  let substitutions, templateId;
+  if (params.formType === 'volunteer') {
+    substitutions = {
+      "full_name": params.fullName,
+      "res_email": params.email,
+      "res_age": params.age,
+      "res_location": params.location,
+      "res_interest": params.interest,
+      "res_help": params.help,
+      "res_skills": params.skills,
+      "res_weeks": params.weeks,
+      "res_months": params.months,
+      "res_experience": params.experience
+    };
+    templateId = process.env.SENDGRID_TEMPLATE_VOLUNTEER;
+  } else {
+    templateId = process.env.SENDGRID_TEMPLATE_FUNDRAISE;
+  }
+  const compose = {
     "personalizations": [{
         "to": [{
-            "email": process.env.RECIPIENT,
-            "name": "Skateistan"
+          "email": process.env.RECIPIENT,
+          "name": "Skateistan"
         }],
-        "substitutions": {
-            "contact_email": params.email,
-            "contact_name": params.name,
-            "contact_message": params.message
-        }
+        "substitutions": substitutions
     }],
     "from": {
-        "email": params.email,
-        "name": params.name
+      "email": params.email,
+      "name": params.fullName
     },
-    "template_id": process.env.SENDGRID_TEMPLATE_KEY
+    "template_id": templateId
   };
 
   return new Promise((fnResolve, fnReject)=>{
@@ -80,9 +92,10 @@ function sendMail(params) {
           .set('Authorization', 'Bearer ' + process.env.SENDGRID_API_KEY)
           .send(compose)
           .end((err, res)=>{
+            console.log('Sendgrid res', res.statusCode);
             if (err) fnReject(err);
             else fnResolve(res);
           });
-      });
+      }).catch((g_res) => console.log('Captcha catch', g_res));
     })
 }
